d8ee4002ea44270ecf3d4bfd2b1d0e44a0cd641a
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2025-07-30 23:30:40 +0900

    add falcon verification precompile contract and update Go module dependencies

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -24,7 +24,7 @@ import (
 	"maps"
 	"math"
 	"math/big"
-
+	"github.com/yhl125/ETHFALCON/falcon"
 	"github.com/consensys/gnark-crypto/ecc"
 	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fp"
@@ -38,6 +38,7 @@ import (
 	"github.com/ethereum/go-ethereum/crypto/secp256r1"
 	"github.com/ethereum/go-ethereum/params"
 	"golang.org/x/crypto/ripemd160"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 )
 
 // PrecompiledContract is the basic interface for native Go contracts. The implementation
@@ -136,6 +137,7 @@ var PrecompiledContractsPrague = PrecompiledContracts{
 	common.BytesToAddress([]byte{0x0f}): &bls12381PairingPrague{},
 	common.BytesToAddress([]byte{0x10}): &bls12381MapG1{},
 	common.BytesToAddress([]byte{0x11}): &bls12381MapG2{},
+	common.BytesToAddress([]byte{0x13}): &falconvrfy{},
 }
 
 var PrecompiledContractsBLS = PrecompiledContractsPrague
@@ -1361,4 +1363,60 @@ func (c *p256Verify) Run(input []byte) ([]byte, error) {
 		// Signature is invalid
 		return nil, nil
 	}
+}
+
+type falconvrfy struct{}
+
+func (c *falconvrfy) RequiredGas(input []byte) uint64 {
+	return 3000
+}
+
+func mustNewBytesType() abi.Type {
+	t, err := abi.NewType("bytes", "", nil)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func (f *falconvrfy) Run(input []byte) ([]byte, error) {
+	args := abi.Arguments{
+		{Type: mustNewBytesType()},
+		{Type: mustNewBytesType()},
+		{Type: mustNewBytesType()},
+	}
+
+
+	fmt.Printf("🔍 Input  (hex): %x\n", input)
+
+	decoded, err := args.Unpack(input)
+	if (err != nil){
+		return nil, errors.New("invalid input ABI")
+	}
+	if (len(decoded) != 3 ){
+		return nil, errors.New("not decoded 3 arguments")
+	}
+
+	sig, ok1 := decoded[0].([]byte)
+	msg, ok2 := decoded[1].([]byte)
+	pub, ok3 := decoded[2].([]byte)
+
+	fmt.Printf("🔍 Signature  (hex): %x\n", sig)
+	fmt.Printf("🔍 Message    (hex): %x\n", msg)
+	fmt.Printf("🔍 Public Key (hex): %x\n", pub)
+
+	if !ok1 || !ok2 || !ok3 {
+		return nil, errors.New("invalid input values")
+	}
+
+	ok, err := falcon.VerifySignature(sig, msg, pub)
+	output := []byte{0}
+	if err != nil {
+		output[0] = 0
+	}
+	
+	if ok {
+		output[0] = 1
+	}
+	return common.LeftPadBytes(output, 32), nil
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/ethereum/go-ethereum
 
-go 1.23.0
+go 1.24.4
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v1.2.0
@@ -65,6 +65,7 @@ require (
 	github.com/supranational/blst v0.3.14
 	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7
 	github.com/urfave/cli/v2 v2.27.5
+	github.com/yhl125/ETHFALCON/falcon v0.1.0
 	go.uber.org/automaxprocs v1.5.2
 	go.uber.org/goleak v1.3.0
 	golang.org/x/crypto v0.35.0
